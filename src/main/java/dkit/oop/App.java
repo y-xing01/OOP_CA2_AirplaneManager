c620ac6d21e528582db50b3d00824922ee58adc1
YueXingTong <d00234628@student.dkit.ie>
2021-12-15 11:39:29 +0000

    Defined field in PassengerAirplane.java Inherit class from Airplane.java Created constructor Defined toString Implement addPassenger() Display PassengerAirplane Object in App.java

--- src/main/java/dkit/oop/App.java
+++ src/main/java/dkit/oop/App.java
@@ -15,27 +15,35 @@ public class App
 
     private void start() {
 
-        System.out.println("OOP - CA2 - 20% - December 2021 - AirplaneManager");
+        System.out.println("OOP - CA2 - 20% - December 2021 - AirplaneManager\n");
 
-        //Q2
-         CargoAirplane cargoPlane1 = new CargoAirplane("Douglas DC-4",20000);
-         CargoAirplane cargoPlane2 = new CargoAirplane("Boeing C-7000",25000);
-        System.out.println(cargoPlane1);
-        System.out.println(cargoPlane2);
+        //Q1
+        CargoAirplane cargoPlane1 = new CargoAirplane("Douglas DC-4",20000);
+        CargoAirplane cargoPlane2 = new CargoAirplane("Boeing C-7000",25000);
 
         // display the two objects
+        System.out.println(cargoPlane1);
+        System.out.println(cargoPlane2);
+        System.out.println();
 
         AirplaneManager mgr = new AirplaneManager();
 
         //Q2
-//         PassengerAirplane passengerPlane1 = new PassengerAirplane("Boeing 747",524);
-//         PassengerAirplane passengerPlane2 = new PassengerAirplane("Airbus 380",525);
-//         //Add passengers to Passenger planes
-//         passengerPlane1.addPassenger("Tom Hardy");
-//         passengerPlane1.addPassenger("Margot Robbie");
-//         passengerPlane2.addPassenger("Emma Thompson");
+         PassengerAirplane passengerPlane1 = new PassengerAirplane("Boeing 747",524);
+         PassengerAirplane passengerPlane2 = new PassengerAirplane("Airbus 380",525);
+        //Add passengers to Passenger planes
+         passengerPlane1.addPassenger("Tom Hardy");
+         passengerPlane1.addPassenger("Margot Robbie");
+         passengerPlane2.addPassenger("Emma Thompson");
+        System.out.println(passengerPlane1);
+        System.out.println(passengerPlane2);
+        System.out.println();
 
         //Q3
+        mgr.add(cargoPlane1);
+        mgr.add(cargoPlane2);
+        mgr.add(passengerPlane1);
+        mgr.add(passengerPlane2);
 
         // write calls to methods implemented for Q3.
 
--- src/main/java/dkit/oop/PassengerAirplane.java
+++ src/main/java/dkit/oop/PassengerAirplane.java
@@ -3,19 +3,31 @@ package dkit.oop;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PassengerAirplane  {
+public class PassengerAirplane extends Airplane {
 
     // fields(Q2)
+    private int MAX_NUM_PASSENGERS;
+    ArrayList<String> passengersList;
 
-    PassengerAirplane(String type, int maxNumPassengers) {
+    PassengerAirplane(String type, int MAX_NUM_PASSENGERS) {
         // code here
-
+        super(type);
+        this.MAX_NUM_PASSENGERS = MAX_NUM_PASSENGERS;
     }
 
     public void addPassenger(String name) {
         // code here
-
+        if(this.passengersList.size() < this.MAX_NUM_PASSENGERS){
+            passengersList.add(name);
+        }
     }
 
-
+    @Override
+    public String toString() {
+        return "PassengerAirplane{" +
+                super.toString() +
+                "MAX_NUM_PASSENGERS=" + MAX_NUM_PASSENGERS +
+                ", passengersList=" + passengersList +
+                '}';
+    }
 } // end of PassengerAirplane
